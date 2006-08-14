9a0d21bde43dd3dff5258bdaaae1016013eb020d
r23 <r23>
2006-08-14 23:45:55 +0000

    update

--- oos-sales/eazysales/dbeS/SetBestellung.php
+++ oos-sales/eazysales/dbeS/SetBestellung.php
@@ -1,6 +1,6 @@
 <?php
 /* ----------------------------------------------------------------------
-   $Id: SetBestellung.php,v 1.15 2006/07/13 04:05:01 r23 Exp $
+   $Id: SetBestellung.php,v 1.16 2006/08/14 23:45:55 r23 Exp $
 
    wawi - osis online shop
 
@@ -40,61 +40,75 @@
 
   $return = 3;
   if (auth()) {
-	$return = 5;
-	//Bestellung versandt
-	if (intval($_POST["action"]) == 6 && intval($_POST['KeyBestellung']))
-	{
-		$return = 0;
-		//setze orders_status auf gew�lte Option bei eS Versadnt
-		//hole einstellungen
-                $eazysales_einstellungenstable = $oostable['eazysales_einstellungen'];
-		$cur_query = xtc_db_query("SELECT StatusVersendet FROM $eazysales_einstellungenstable");
-		$einstellungen = mysql_fetch_object($cur_query);
-		
-		//setze status der Bestellung
-		if ($einstellungen->StatusVersendet>0)
-		{
+    $return = 5;
 
-                  $orderstable = $oostable['orders'];
-			xtc_db_query("UPDATE $orderstable SET orders_status=".$einstellungen->StatusVersendet." WHERE orders_id=".intval($_POST['KeyBestellung']));
-			//fge history hinzu
+    //Bestellung versandt
+    if (intval($_POST["action"]) == 6 && intval($_POST['KeyBestellung'])) {
+      $return = 0;
+
+       //hole einstellungen
+      $eazysales_einstellungenstable = $oostable['eazysales_einstellungen'];
+      $query = "SELECT StatusVersendet FROM $eazysales_einstellungenstable");
+      $status_versendet = $dbconn->GetOne($query);
+
+      //setze status der Bestellung
+      if ($status_versendet>0) {
+        $orderstable = $oostable['orders'];
+        $dbconn->Execute("UPDATE $orderstable SET orders_status=".$status_versendet." WHERE orders_id=".intval($_POST['KeyBestellung']));
+
 			$VersandInfo = realEscape($_POST["VersandInfo"]);
 			$VersandDatum = realEscape($_POST["VersandDatum"]);
 			$Tracking = realEscape($_POST["Tracking"]);
 			$kommentar = "Bestellung aus eazySales am $VersandDatum versandt.\n".$VersandInfo."\nIdentCode".$Tracking;
 
-                  $orders_status_historytable = $oostable['orders_status_history'];
-			xtc_db_query("INSERT INTO $orders_status_historytable (orders_id, orders_status_id, date_added, comments) values(".intval($_POST['KeyBestellung']).", ".$einstellungen->StatusVersendet.", now(), \"".$kommentar."\")");
-		}
- 	}
+        $orders_status_historytable = $oostable['orders_status_history'];
+        $dbconn->Execute("INSERT INTO $orders_status_historytable 
+                         (orders_id,
+                          orders_status_id,
+                          date_added,
+                          comments) VALUES(".intval($_POST['KeyBestellung']).",
+                                           ".$status_versendet.",
+                                            now(),
+                                           \"".$kommentar."\")");
+      }
+    }
 
-	//Bestellung erfolgreich abgeholt
-	if (intval($_POST["action"]) == 5 && intval($_POST['KeyBestellung']))
-	{
-		$return = 0;
-		//setze orders_status auf gew�lte Option bei eS Abholung
-		//hole einstellungen
-		$cur_query = xtc_db_query("SELECT StatusAbgeholt FROM eazysales_einstellungen");
-		$einstellungen = mysql_fetch_object($cur_query);
-		
-		//setze status der Bestellung
-		if ($einstellungen->StatusAbgeholt>0)
-		{
+    //Bestellung erfolgreich abgeholt
+    if (intval($_POST["action"]) == 5 && intval($_POST['KeyBestellung'])) {
+      $return = 0;
 
-                  $orderstable = $oostable['orders'];
-			xtc_db_query("UPDATE $orderstable SET orders_status=".$einstellungen->StatusAbgeholt." WHERE orders_id=".intval($_POST['KeyBestellung']));
-			//fge history hinzu
-			$kommentar = "Erfolgreich in eazySales bernommen";
+      //hole einstellungen
+      $eazysales_einstellungenstable = $oostable['eazysales_einstellungen'];
+      $query = "SELECT StatusAbgeholt FROM $eazysales_einstellungenstable");
+      $status_abgeholt = $dbconn->GetOne($query);
 
-                  $orders_status_historytable = $oostable['orders_status_history'];
-			xtc_db_query("INSERT INTO $orders_status_historytable (orders_id, orders_status_id, date_added, comments) values(".intval($_POST['KeyBestellung']).", ".$einstellungen->StatusAbgeholt.", now(), \"".$kommentar."\")");
-		}
-		
-		//setze bestellung auf abgeholt
-                $eazysales_sentorderstable = $oostable['eazysales_sentorders'];
-		xtc_db_query("INSERT INTO $eazysales_sentorderstable (orders_id, dGesendet) VALUES (".intval($_POST['KeyBestellung']).",now())");
-	}
-}
+      //setze status der Bestellung
+      if ($status_abgeholt > 0) {
+
+        $orderstable = $oostable['orders'];
+        $dbconn->Execute("UPDATE $orderstable SET orders_status=".$status_abgeholt." WHERE orders_id=".intval($_POST['KeyBestellung']));
+        //fuege history hinzu
+        $kommentar = "Erfolgreich in eazySales bernommen";
+
+        $orders_status_historytable = $oostable['orders_status_history'];
+        $dbconn->Executey("INSERT INTO $orders_status_historytable 
+                           (orders_id,
+                            orders_status_id,
+                            date_added,
+                            comments) VALUES(".intval($_POST['KeyBestellung']).",
+                                              ".$einstellungen->StatusAbgeholt.",
+                                               now(),
+                                                \"".$kommentar."\")");
+      }
+
+      //setze bestellung auf abgeholt
+      $eazysales_sentorderstable = $oostable['eazysales_sentorders'];
+      $dbconn->Executey("INSERT INTO $eazysales_sentorderstable 
+                        (orders_id,
+                         dGesendet) VALUES (".intval($_POST['KeyBestellung']).",
+                                              now())");
+    }
+  }
 
   echo($return);
   logge($return);
